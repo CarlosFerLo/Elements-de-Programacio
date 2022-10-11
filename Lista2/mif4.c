d072ce2eede95e9df4404adc01f91988a4a549ba
Carlos Fernández <67836662+CarlosFerLo@users.noreply.github.com>
2022-10-11 17:30:17 +0200

    Add files via upload

---
+++ Lista2/mif4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) {
+	int x0, x1, x2, x3, y0, y1, y2, y3, contador = 0, m, f ;
+	
+	srand(time(NULL)) ;
+
+	y0 = rand() % 10 ;
+	y1 = rand() % 10 ;
+	y2 = rand() % 10 ;
+	y3 = rand() % 10 ;
+	
+	do {
+		contador++ ;
+		m = f = 0 ;
+		printf("%2d) Introduce 4 digitos: ", contador) ;
+		scanf(" %d %d %d %d", &x0, &x1, &x2, &x3) ;
+
+		if (x0 == y0) m++ ;
+		else if (x0 == y1 || x0 == y2 || x0 == y3) f++ ;
+		if (x1 == y1) m++ ;
+		else if (x1 == y0 || x1 == y2 || x1 == y3) f++ ;
+		if (x2 == y2) m++ ;
+		else if (x2 == y1 || x2 == y0 || x2 == y3) f++ ;
+		if (x3 == y3) m++ ;
+		else if (x3 == y1 || x3 == y2 || x3 == y0) f++ ;
+
+		if (m == 4) printf("Has ganado en %d intentos!\n", contador) ;
+		else printf("\tm = %d f = %d\n", m, f) ;
+ 
+	} while (!(x0 == y0 && x1 == y1 && x2 == y2 && x3 == y3)) ;
+
+	return 0 ;
+}
